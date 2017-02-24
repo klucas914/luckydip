9c5b333f074c5557e2f313aa0970775e96713917
Kristin Lucas <klucas914@gmail.com>
2017-02-24 17:41:20 +1000

    changed logo on most pages and altered some minor styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require chosen-jquery
 //= require_tree .
